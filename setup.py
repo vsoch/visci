c25f35cdc192e7a7e9cb11565ab7f8fb557e74e9
vsoch <vsochat@stanford.edu>
2015-12-01 13:51:11 -0800

    first commit

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    # Application name:
+    name="visci",
+
+    # Version number (initial):
+    version="0.1",
+
+    # Application author details:
+    author="Vanessa Sochat",
+    author_email="vsochat@stanford.edu",
+
+    # Packages
+    packages=find_packages(),
+
+    # Details
+    url="http://www.github.com/vsoch/visualization-ci",
+
+    license="LICENSE.txt",
+    description="visualization standard file structure for continuous integration",
+
+)
